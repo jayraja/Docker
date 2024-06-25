6599d85ba8ca0802bec846f8a18b055e497ae0ce
Jay Raja <jayraja15@hotmail.com>
2024-06-25 11:40:35 +0100

    WIP - End of Dealing with multiple containers

--- DockerCourseApi/Program.cs
+++ DockerCourseApi/Program.cs
@@ -1,3 +1,4 @@
+using Dapper;
 using System.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,11 +9,16 @@ var app = builder.Build();
 app.UseCors(x => x.AllowAnyOrigin());
 
 
-app.MapGet("/podcasts", () =>
+app.MapGet("/podcasts", async () =>
 {
-    var dbConnection = new SqlConnection("")
+    var db = new SqlConnection("Server=database;Database=podcasts;User Id=sa;Password=Dometrain#123;");
 
+    var list = await db.QueryAsync<Podcast>("Select * from Podcasts");
+    var newList = list.Select(l => l.Title).ToList();
 
+    return newList;
 });
 
 app.Run();
+
+public record Podcast(Guid Id, string Title);
--- Readme.txt
+++ Readme.txt
@@ -4,4 +4,17 @@ Course Notes
 - Download Azure Data Studio or another client
 - Run the following on the command line to pull Ms SQL
 docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Dometrain#123" -p 1433:1433 mcr.microsoft.com/mssql/server:2022-latest
+
+create database podcasts
+
+create table Podcasts
+(
+    Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+    Title NVARCHAR(MAX) NOT NULL
+)
+
+INSERT INTO Podcasts (Title)
+VALUES
+('Database podcast'),
+('Docker podcast');
  
